1ed22b92be926eac6f9100b9b4265aca8bb06882
jaeungKim <kju2405@gmail.com>
2023-08-31 16:52:15 +0900

    Feat: 상품 엔티티 구현 [#3]

---
+++ src/main/java/com/programmers/dev/kream/product/domain/SizedProduct.java
@@ -0,0 +1,27 @@
+package com.programmers.dev.kream.product.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "SIZED_PRODUCTS")
+public class SizedProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    @Column(name = "SIZE")
+    private int size;
+
+    protected SizedProduct() {}
+
+    public SizedProduct(Product product, int size) {
+        this.product = product;
+        this.size = size;
+    }
+}
