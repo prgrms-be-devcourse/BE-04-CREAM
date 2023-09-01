9054b9157d11769dc474ad05639989ef19e9e7bf
Jaeung Kim <kju9730@gmail.com>
2023-09-01 13:40:55 +0900

    Feat: Repository 구현

---
+++ src/main/java/com/programmers/dev/kream/product/domain/BrandRepository.java
@@ -0,0 +1,7 @@
+package com.programmers.dev.kream.product.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    
+}
---
+++ src/main/java/com/programmers/dev/kream/user/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package com.programmers.dev.kream.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
