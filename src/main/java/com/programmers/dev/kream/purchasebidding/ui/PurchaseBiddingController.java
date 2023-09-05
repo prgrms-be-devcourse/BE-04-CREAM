f46b28943cb30d397e77f0a3276180249a81fdfd
PHJ <legowww@gmail.com>
2023-09-05 17:05:23 +0900

    Refactor: release 1.0 배포 전 코드 리팩토링 수행 [#35]

--- src/main/java/com/programmers/dev/kream/purchasebidding/ui/PurchaseBiddingController.java
+++ src/main/java/com/programmers/dev/kream/purchasebidding/ui/PurchaseBiddingController.java
@@ -29,11 +29,6 @@ public class PurchaseBiddingController {
         this.productService = productService;
     }
 
-    /**
-     * [상품 구입 선택 화면]
-     * @param productId : 구입하고 싶은 제품번호
-     * @return : 제품이름, 사이즈 별 최저가
-     */
     @GetMapping("/{productId}")
     public ResponseEntity<PurchaseSelectView> getProductSelectView(@PathVariable Long productId) {
         ProductResponse product = productService.findById(productId);
@@ -42,11 +37,6 @@ public class PurchaseBiddingController {
         return ResponseEntity.ok(new PurchaseSelectView(product.name(), biddingSelectLines));
     }
 
-    /**
-     * [즉시 구매]
-     * @param request : 구매가격, 상품번호
-     * @return : 구매입찰ID
-     */
     @PostMapping("/purchase-now")
     public ResponseEntity<PurchaseBiddingResponse> purchaseNow(@RequestBody PurchaseBiddingNowRequest request) {
         Long purchaseBiddingId = purchaseBiddingService.purchaseNow(getPurchaserId(), request);
@@ -54,11 +44,6 @@ public class PurchaseBiddingController {
         return ResponseEntity.ok(new PurchaseBiddingResponse(purchaseBiddingId));
     }
 
-    /**
-     * [입찰 구매]
-     * @param request : 입찰희망가격, 상품번호, 입찰기간
-     * @return : 구매입찰ID
-     */
     @PostMapping("/bid")
     public ResponseEntity<PurchaseBiddingResponse> bid(@RequestBody PurchaseBiddingBidRequest request) {
         Long purchaseBiddingId = purchaseBiddingService.bid(getPurchaserId(), request);
