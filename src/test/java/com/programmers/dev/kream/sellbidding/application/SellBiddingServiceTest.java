eda271707c7dfbd11ca0d9048564b41164785013
BeomSeogKim <kbs4520@daum.net>
2023-09-08 17:17:53 +0900

    Test: SizedProduct 제거로 인한 테스트 코드 수정 [#52]

--- src/test/java/com/programmers/dev/kream/sellbidding/application/SellBiddingServiceTest.java
+++ src/test/java/com/programmers/dev/kream/sellbidding/application/SellBiddingServiceTest.java
@@ -35,9 +35,6 @@ class SellBiddingServiceTest {
     UserRepository userRepository;
 
     @Autowired
-    SizedProductRepository sizedProductRepository;
-
-    @Autowired
     SellBiddingRepository sellBiddingRepository;
 
     @Autowired
@@ -57,39 +54,42 @@ class SellBiddingServiceTest {
     void getProductInformation() {
         // given
         User user = makeUser("email.com", "조단 사랑");
-        Product product = makeProduct("nike", "Air Jordan");
-        SizedProduct sizedProduct1 = makeSizedProduct(product, 255);
-        SizedProduct sizedProduct2 = makeSizedProduct(product, 260);
-        SizedProduct sizedProduct3 = makeSizedProduct(product, 265);
-        makePurchaseBidding(user, sizedProduct1);
-        makePurchaseBidding(user, sizedProduct2);
+        Product product1 = makeProduct("nike", "Air Jordan",255);
+        Product product2 = makeProduct("nike", "Air Jordan",260);
+        Product product3 = makeProduct("nike", "Air Jordan",265);
+        makePurchaseBidding(user, product1);
+        makePurchaseBidding(user, product2);
         // when
-        ProductInformation productInformation = sellBiddingService.getProductInformation(sizedProduct1.getProduct().getId());
+        ProductInformation productInformation = sellBiddingService.getProductInformation(product1.getName(), product1.getBrand().getName());
 
         // then
         assertAll(
-                () -> assertThat(productInformation.productName()).isEqualTo(sizedProduct1.getProduct().getName()),
+                () -> assertThat(productInformation.productName()).isEqualTo(product1.getName()),
                 () -> assertThat(productInformation.biddingSelectLines().size()).isEqualTo(3),
                 () -> assertThat(productInformation.biddingSelectLines().get(2).lived()).isEqualTo(false)
         );
     }
 
     @Test
-    @DisplayName("판매입찰 페이지 조회시 잘못된 id로 조회할 경우 예외가 발생해야 한다")
+    @DisplayName("판매입찰 페이지 조회시 잘못된 id로 조회할 경우 biddingSelectLines에는 정보가 없어야 한다")
     void getProductInformation_InvalidProductId() {
         // given
         User user = makeUser("email.com", "조단 사랑");
-        Product product = makeProduct("nike", "Air Jordan");
-        SizedProduct sizedProduct1 = makeSizedProduct(product, 255);
-        SizedProduct sizedProduct2 = makeSizedProduct(product, 260);
-        SizedProduct sizedProduct3 = makeSizedProduct(product, 265);
-        makePurchaseBidding(user, sizedProduct1);
-        makePurchaseBidding(user, sizedProduct2);
+        Product product1 = makeProduct("nike", "Air Jordan", 255);
+        Product product2 = makeProduct("nike", "Air Jordan", 260);
+        Product product3 = makeProduct("nike", "Air Jordan", 265);
+        makePurchaseBidding(user, product1);
+        makePurchaseBidding(user, product2);
 
         // when && then
-        assertThatThrownBy(
-                () -> sellBiddingService.getProductInformation(sizedProduct1.getProduct().getId() + 1)
-        ).isInstanceOf(CreamException.class);
+
+        ProductInformation productInformation = sellBiddingService.getProductInformation(product1.getName() + "alpha", product1.getBrand().getName());
+
+        assertAll(
+                () -> assertThat(productInformation.productName()).isEqualTo("Air Jordanalpha"),
+                () -> assertThat(productInformation.biddingSelectLines().size()).isEqualTo(0)
+        );
+
     }
 
     @Test
@@ -97,14 +97,13 @@ class SellBiddingServiceTest {
     void saveSellBidding() {
         // given
         User user = makeUser("email.com", "조단 사랑");
-        Product product = makeProduct("nike", "Air Jordan");
-        SizedProduct sizedProduct = makeSizedProduct(product, 255);
+        Product product = makeProduct("nike", "Air Jordan", 255);
         SellBiddingRequest sellBiddingRequest = new SellBiddingRequest(190000, 30L);
 
-        // when
-        SellBiddingResponse sellBiddingResponse = sellBiddingService.saveSellBidding(user.getId(), sizedProduct.getId(), sellBiddingRequest);
+         // when
+        SellBiddingResponse sellBiddingResponse = sellBiddingService.saveSellBidding(user.getId(), product.getId(), sellBiddingRequest);
 
-        // then
+         // then
         SellBidding savedSellBidding = sellBiddingRepository.findById(sellBiddingResponse.sellBiddingId()).orElseThrow();
         assertAll(
                 () -> assertThat(savedSellBidding.getSellBidderId()).isEqualTo(user.getId()),
@@ -118,17 +117,16 @@ class SellBiddingServiceTest {
     void saveSellBiddingInvalidSizedProductId() {
         // given
         User user = makeUser("naver.com", "tommy");
-        Product product = makeProduct("adidas", "푸마");
-        SizedProduct sizedProduct = makeSizedProduct(product, 300);
+        Product product = makeProduct("adidas", "푸마", 300);
         SellBiddingRequest sellBiddingRequest = new SellBiddingRequest(190000, 30L);
 
         // when && then
         assertThatThrownBy(
-                () -> sellBiddingService.saveSellBidding(user.getId(), sizedProduct.getId() + 1, sellBiddingRequest)
+                () -> sellBiddingService.saveSellBidding(user.getId(), product.getId() + 1, sellBiddingRequest)
         ).isInstanceOf(CreamException.class);
 
         assertThatThrownBy(
-                () -> sellBiddingService.saveSellBidding(user.getId() + 1, sizedProduct.getId(), sellBiddingRequest)
+                () -> sellBiddingService.saveSellBidding(user.getId() + 1, product.getId(), sellBiddingRequest)
         ).isInstanceOf(CreamException.class);
 
     }
@@ -139,9 +137,8 @@ class SellBiddingServiceTest {
         // given
         User user1 = makeUser("naver.com", "tommy");
         User user2 = makeUser("google.com", "axis");
-        Product product = makeProduct("Nike", "air jordan");
-        SizedProduct sizedProduct = makeSizedProduct(product, 260);
-        PurchaseBidding purchaseBidding = makePurchaseBidding(user1, sizedProduct);
+        Product product = makeProduct("Nike", "air jordan", 260);
+        PurchaseBidding purchaseBidding = makePurchaseBidding(user1, product);
 
         // when
         SellBiddingResponse sellBiddingResponse = sellBiddingService.transactPurchaseBidding(user2.getId(), purchaseBidding.getId());
@@ -168,9 +165,8 @@ class SellBiddingServiceTest {
     void transactPurchaseBidding_InvalidPurchase() {
         // given
         User user1 = makeUser("naver.com", "tommy");
-        Product product = makeProduct("Nike", "air jordan");
-        SizedProduct sizedProduct = makeSizedProduct(product, 260);
-        PurchaseBidding purchaseBidding = makePurchaseBidding(user1, sizedProduct);
+        Product product = makeProduct("Nike", "air jordan", 260);
+        PurchaseBidding purchaseBidding = makePurchaseBidding(user1, product);
 
         // when && then
         assertThatThrownBy(
@@ -185,26 +181,18 @@ class SellBiddingServiceTest {
         return user;
     }
 
-    private Product makeProduct(String brandName, String productName) {
+    private Product makeProduct(String brandName, String productName, int size) {
         Brand nike = new Brand(brandName);
         brandRepository.save(nike);
         ProductInfo productInfo = new ProductInfo("A-1202020", LocalDateTime.now().minusDays(100), "RED", 20000L);
-        Product product = new Product(nike, productName, productInfo);
+        Product product = new Product(nike, productName, productInfo, size);
         productRepository.save(product);
 
         return product;
     }
 
-
-    private SizedProduct makeSizedProduct(Product product, int size) {
-        SizedProduct sizedProduct = new SizedProduct(product, size);
-        sizedProductRepository.save(sizedProduct);
-
-        return sizedProduct;
-    }
-
-    private PurchaseBidding makePurchaseBidding(User user, SizedProduct sizedProduct) {
-        PurchaseBidding purchaseBidding = new PurchaseBidding(user.getId(), sizedProduct.getId(), 20000L, Status.LIVE, LocalDateTime.now(), LocalDateTime.now().plusDays(20));
+    private PurchaseBidding makePurchaseBidding(User user, Product product) {
+        PurchaseBidding purchaseBidding = new PurchaseBidding(user.getId(), product.getId(), 20000L, Status.LIVE, LocalDateTime.now(), LocalDateTime.now().plusDays(20));
         purchaseBiddingRepository.save(purchaseBidding);
         return purchaseBidding;
     }
