625f0abe4092769e5e4c64da6f6585323e51f03b
PHJ <legowww@gmail.com>
2023-09-14 00:25:33 +0900

    Chore: Lombok 선언

--- module-domain/src/main/java/com/programmers/dev/common/Status.java
+++ module-domain/src/main/java/com/programmers/dev/common/Status.java
@@ -2,6 +2,7 @@ package com.programmers.dev.common;
 
 /**
  * LIVE : 입찰 중
+ * OUT_WAREHOUSE : 입고 대기
  * IN_WAREHOUSE : 입고 완료
  * AUTHENTICATED : 검수 합격
  * DELIVERING : 배송 중
@@ -14,7 +15,11 @@ public enum Status {
 
     LIVE,
 
+    OUT_WAREHOUSE,
+
     IN_WAREHOUSE,
+
+    AUTHENTICATING,
 
     AUTHENTICATED,
 
