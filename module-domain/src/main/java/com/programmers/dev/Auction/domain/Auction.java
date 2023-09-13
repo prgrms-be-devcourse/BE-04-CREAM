32df2a0a4a457f9d5a5ef981329b59d56a153a77
Jaeung Kim <kju9730@gmail.com>
2023-09-13 15:16:27 +0900

    Feat: Auction, AuctionBidding 엔티티 및 Repository생성 [#80]

---
+++ module-domain/src/main/java/com/programmers/dev/Auction/domain/Auction.java
@@ -0,0 +1,61 @@
+package com.programmers.dev.Auction.domain;
+
+import com.programmers.dev.product.domain.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "AUCTIONS")
+@Getter
+public class Auction {
+
+    enum AuctionStatus {
+        BEFORE,
+        ONGOING,
+        FINISHED
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    @Column(name = "START_PRICE", nullable = false)
+    private Long startPrice;
+
+    @Column(name = "START_TIME", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "END_TIME", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(name = "AUCTION_STATUS", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AuctionStatus auctionStatus;
+
+    @Column(name = "BIDDER_ID")
+    private Long bidderId;
+
+    @Column(name = "PRICE")
+    private Long price;
+
+    protected Auction() {
+
+    }
+
+    public Auction(Product product, Long startPrice, LocalDateTime startTime, LocalDateTime endTime, AuctionStatus auctionStatus, Long bidderId, Long price) {
+        this.product = product;
+        this.startPrice = startPrice;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.auctionStatus = auctionStatus;
+        this.bidderId = bidderId;
+        this.price = price;
+    }
+}
---
+++ module-domain/src/main/java/com/programmers/dev/Auction/domain/AuctionBiddingRepository.java
@@ -0,0 +1,7 @@
+package com.programmers.dev.Auction.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuctionBiddingRepository extends JpaRepository<AuctionBidding, Long> {
+
+}
