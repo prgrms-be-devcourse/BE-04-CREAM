3358a95e12c80b02141164aae81c732a20931fed
PHJ <legowww@gmail.com>
2023-09-16 16:40:40 +0900

    Test: 보관 판매 컨트롤러 테스트 작성 및 리팩토링

---
+++ module-api/src/test/java/com/programmers/dev/inventory/ui/InventoryStateChangeControllerAuthenticationTest.java
@@ -0,0 +1,175 @@
+package com.programmers.dev.inventory.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.programmers.dev.common.PenaltyType;
+import com.programmers.dev.common.Status;
+import com.programmers.dev.inventory.domain.Inventory;
+import com.programmers.dev.inventory.domain.InventoryRepository;
+import com.programmers.dev.inventory.dto.InventoryRegisterRequest;
+import com.programmers.dev.inventory.dto.statechange.InventoryArrivedRequest;
+import com.programmers.dev.inventory.dto.statechange.InventoryAuthenticateFailRequest;
+import com.programmers.dev.inventory.dto.statechange.InventoryAuthenticatePassRequest;
+import com.programmers.dev.product.domain.*;
+import com.programmers.dev.security.jwt.JwtConfigure;
+import com.programmers.dev.security.jwt.JwtTokenUtils;
+import com.programmers.dev.user.domain.Address;
+import com.programmers.dev.user.domain.User;
+import com.programmers.dev.user.domain.UserRepository;
+import com.programmers.dev.user.domain.UserRole;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@Transactional
+@ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class InventoryStateChangeControllerAuthenticationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private JwtConfigure jwtConfigure;
+
+    @BeforeEach
+    void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentationContextProvider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .apply(documentationConfiguration(restDocumentationContextProvider)
+                        .operationPreprocessors()
+                        .withRequestDefaults(modifyUris().host("13.125.254.94"), prettyPrint())
+                        .withResponseDefaults(modifyUris().host("13.125.254.94"), prettyPrint()))
+                .build();
+    }
+
+    @Test
+    @DisplayName("요청한 inventoryId 상품이 검수에 합격하면 inventoryId를 반환받는다.")
+    void 검수_합격() throws Exception {
+        //given
+        User user = createUser();
+        String accessToken = getAccessToken(user.getId(), user.getUserRole());
+        Product product = createProduct();
+        Inventory inventory = createInventory(user.getId(), product.getId(), user.getAddress());
+
+        //when && then
+        InventoryAuthenticatePassRequest request = new InventoryAuthenticatePassRequest(Inventory.ProductQuality.COMPLETE);
+        mockMvc.perform(post("/api/inventories/state-change/authentication/{inventoryId}/passed", inventory.getId())
+                        .header(HttpHeaders.AUTHORIZATION, accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andDo(document("inventoriy-authentication-passed",
+                        requestHeaders(
+                                headerWithName(CONTENT_TYPE).description("content type"),
+                                headerWithName(CONTENT_LENGTH).description("content length")
+                        ),
+                        requestFields(
+                                fieldWithPath("productQuality").description("productQuality of authentication")
+                        ),
+                        responseFields(
+                                fieldWithPath("inventoryId").description("id of inventory")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("요청한 inventoryId 상품이 검수에 실패하면 inventoryId를 반환받는다.")
+    void 검수_실패() throws Exception {
+        User user = createUser();
+        String accessToken = getAccessToken(user.getId(), user.getUserRole());
+        Product product = createProduct();
+
+        Inventory inventory = createInventory(user.getId(), product.getId(), user.getAddress());
+        InventoryAuthenticateFailRequest request = new InventoryAuthenticateFailRequest(PenaltyType.PRODUCT_DAMEGED);
+
+        mockMvc.perform(post("/api/inventories/state-change/authentication/{inventoryId}/failed", inventory.getId())
+                        .header(HttpHeaders.AUTHORIZATION, accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andDo(document("inventoriy-authentication-failed",
+                        requestHeaders(
+                                headerWithName(CONTENT_TYPE).description("content type"),
+                                headerWithName(CONTENT_LENGTH).description("content length")
+                        ),
+                        requestFields(
+                                fieldWithPath("penaltyType").description("penaltyType of authentication")
+                        ),
+                        responseFields(
+                                fieldWithPath("inventoryId").description("id of inventory")
+                        )
+                ));
+    }
+
+    private String getAccessToken(Long userId, UserRole userRole) {
+        return "Bearer " + JwtTokenUtils.generateAccessToken(String.valueOf(userId), userRole.toString(), jwtConfigure.getSecretKey(), jwtConfigure.getAccessTokenExpiryTimeMs());
+    }
+
+    private User createUser() {
+        return userRepository.save(new User("test@email.com", "test", "sellUser", 10_000L, new Address("00001", "인천", "연수구"), UserRole.ROLE_USER));
+    }
+
+    private Product createProduct() {
+        Brand brand = new Brand("ADIDAS");
+        brandRepository.save(brand);
+
+        ProductInfo productInfo = new ProductInfo("ADI-001", LocalDateTime.now(), "BLACK", 50_000L);
+        Product product = new Product(brand, "SUPER-STAR", productInfo, 250);
+
+        return productRepository.save(product);
+    }
+
+    private Inventory createInventory(Long userId, Long productId, Address address) {
+        Inventory inventory = new Inventory(userId, productId, Inventory.InventoryType.SELL, Status.IN_WAREHOUSE, address, LocalDateTime.now());
+
+        return inventoryRepository.save(inventory);
+    }
+}
--- module-api/src/test/java/com/programmers/dev/inventory/ui/InventoryStateChangeControllerSetPriceTest.java
+++ module-api/src/test/java/com/programmers/dev/inventory/ui/InventoryStateChangeControllerSetPriceTest.java
@@ -1,11 +1,14 @@
 package com.programmers.dev.inventory.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.programmers.dev.common.Status;
+import com.programmers.dev.inventory.domain.Inventory;
+import com.programmers.dev.inventory.domain.InventoryRepository;
 import com.programmers.dev.inventory.dto.InventoryRegisterRequest;
-
+import com.programmers.dev.inventory.dto.statechange.InventorySetPriceRequest;
 import com.programmers.dev.product.domain.*;
-import com.programmers.dev.security.jwt.*;
+import com.programmers.dev.security.jwt.JwtConfigure;
+import com.programmers.dev.security.jwt.JwtTokenUtils;
 import com.programmers.dev.user.domain.Address;
 import com.programmers.dev.user.domain.User;
 import com.programmers.dev.user.domain.UserRepository;
@@ -46,13 +49,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @ExtendWith(RestDocumentationExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class InventoryControllerTest {
+class InventoryStateChangeControllerSetPriceTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -79,35 +85,33 @@ class InventoryControllerTest {
     }
 
     @Test
-    @DisplayName("보관판매 신청을 하면 보관판매 ID를 반환받는다.")
-    void 보관판매에_성공하면_생성된_보관판매_ID를_반환받는다() throws Exception {
-        User user = createUserHavingMoney(10_000L);
+    @DisplayName("요청한 inventoryId 상품에 대한 판매희망가를 입력하면 inventoryId를 반환받는다.")
+    void 판매희망가_입력() throws Exception {
+        //given
+        User user = createUser();
         String accessToken = getAccessToken(user.getId(), user.getUserRole());
-        Product product = getTargetProduct();
+        Product product = createProduct();
+        Long hopedPrice = 100_000L;
+        Inventory inventory = createInventory(user.getId(), product.getId(), user.getAddress());
 
-        InventoryRegisterRequest request = crateStoreRequest(product.getId(), 3L, user.getAddress());
-
-
-        mockMvc.perform(post("/api/inventories/register")
+        //when && then
+        InventorySetPriceRequest request = new InventorySetPriceRequest(hopedPrice);
+        mockMvc.perform(post("/api/inventories/state-change/{inventoryId}/set-price", inventory.getId())
                         .header(HttpHeaders.AUTHORIZATION, accessToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request))
                 )
                 .andDo(print())
-                .andDo(document("inventory-store",
+                .andDo(document("inventory-set-price",
                         requestHeaders(
                                 headerWithName(CONTENT_TYPE).description("content type"),
                                 headerWithName(CONTENT_LENGTH).description("content length")
                         ),
                         requestFields(
-                                fieldWithPath("productId").description("id of product"),
-                                fieldWithPath("quantity").description("price of bidding"),
-                                fieldWithPath("returnZipcode").description("returnZipcode of inventory"),
-                                fieldWithPath("returnAddress").description("returnAddress of inventory"),
-                                fieldWithPath("returnAddressDetail").description("returnAddressDetail of inventory")
+                                fieldWithPath("hopedPrice").description("hopedPrice of inventory")
                         ),
                         responseFields(
-                                fieldWithPath("inventoryIds").description("created inventoryIds")
+                                fieldWithPath("inventoryId").description("id of inventory")
                         )
                 ));
     }
@@ -116,11 +120,11 @@ class InventoryControllerTest {
         return "Bearer " + JwtTokenUtils.generateAccessToken(String.valueOf(userId), userRole.toString(), jwtConfigure.getSecretKey(), jwtConfigure.getAccessTokenExpiryTimeMs());
     }
 
-    private User createUserHavingMoney(Long account) {
-        return userRepository.save(new User("test@email.com", "test", "sellUser", account, new Address("00001", "인천", "연수구"), UserRole.ROLE_USER));
+    private User createUser() {
+        return userRepository.save(new User("test@email.com", "test", "sellUser", 10_000L, new Address("00001", "인천", "연수구"), UserRole.ROLE_USER));
     }
 
-    private Product getTargetProduct() {
+    private Product createProduct() {
         Brand brand = new Brand("ADIDAS");
         brandRepository.save(brand);
 
@@ -130,7 +134,9 @@ class InventoryControllerTest {
         return productRepository.save(product);
     }
 
-    private InventoryRegisterRequest crateStoreRequest(Long productId, Long quantity, Address address) {
-        return new InventoryRegisterRequest(productId, quantity, address.getZipcode(), address.getAddress(), address.getAddressDetail());
+    private Inventory createInventory(Long userId, Long productId, Address address) {
+        Inventory inventory = new Inventory(userId, productId, Inventory.InventoryType.SELL, Status.AUTHENTICATED, address, LocalDateTime.now());
+
+        return inventoryRepository.save(inventory);
     }
 }
