985d90c32f6a7c6f07890651b481b3262d24cc11
PHJ <legowww@gmail.com>
2023-09-17 16:21:46 +0900

    Feat: InventoryOrder 엔티티 및 레포지토리 구현

---
+++ module-domain/src/main/java/com/programmers/dev/inventoryorder/domain/InventoryOrder.java
@@ -0,0 +1,55 @@
+package com.programmers.dev.inventoryorder.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "INVENTORY_ORDERS")
+@Getter
+public class InventoryOrder {
+
+    enum InventoryOrderStatus {
+        DELIVERY_WAITING,
+
+        DELIVERING,
+
+        SHIPPED,
+
+        FINISHED,
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "USER_ID", nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(name = "INVENTORY_ID", nullable = false, updatable = false)
+    private Long inventoryId;
+
+    @Column(name = "ORDERD_PRICE", nullable = false, updatable = false)
+    private Long orderdPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS", nullable = false)
+    private InventoryOrderStatus inventoryOrderStatus;
+
+    @Column(name = "TRANSACTION_DATE", updatable = false)
+    private LocalDateTime transactionDate;
+
+    protected InventoryOrder() {
+    }
+
+    public InventoryOrder(Long userId, Long inventoryId, Long orderdPrice, LocalDateTime transactionDate) {
+        this.userId = userId;
+        this.inventoryId = inventoryId;
+        this.orderdPrice = orderdPrice;
+        this.inventoryOrderStatus = InventoryOrderStatus.DELIVERY_WAITING;
+        this.transactionDate = transactionDate;
+    }
+}
