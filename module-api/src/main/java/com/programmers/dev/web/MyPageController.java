5536b8561b20850ce7f5a973c6af1c105c1b53d0
PHJ <legowww@gmail.com>
2023-09-21 18:18:57 +0900

    Feat: status 전용 화면 정산 및 계좌 현황 추가

--- module-api/src/main/java/com/programmers/dev/web/MyPageController.java
+++ module-api/src/main/java/com/programmers/dev/web/MyPageController.java
@@ -1,6 +1,11 @@
 package com.programmers.dev.web;
 
+import com.programmers.dev.settlement.application.SettlementService;
+import com.programmers.dev.settlement.domain.Settlement;
+import com.programmers.dev.settlement.domain.SettlementRepository;
+import com.programmers.dev.settlement.query.dto.SettlementView;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +18,8 @@ public class MyPageController {
 
     private final MyPageService myPageService;
 
+    private final SettlementRepository settlementRepository;
+
     @GetMapping("/web/myPage")
     public String myPage(Model model) {
         MyPageService.UserViewDto purchaser = myPageService.getPurchaser();
@@ -20,10 +27,14 @@ public class MyPageController {
         List<MyPageService.ProductView> purchaserProductViews = myPageService.selectAllForPurchaser();
         List<MyPageService.ProductView> sellerProductViews = myPageService.selectAllForSeller();
 
+        List<Settlement> settlements = settlementRepository.findAll();
+
+
         model.addAttribute("purchaser", purchaser);
         model.addAttribute("seller", seller);
         model.addAttribute("purchaserViews", purchaserProductViews);
         model.addAttribute("sellerViews", sellerProductViews);
+        model.addAttribute("settlements", settlements);
 
         return "myPage";
     }
