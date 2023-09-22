aefce9159df5a18d237cac0d055ef32a46fb6b02
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2023-09-22 18:14:03 +0900

    Feat: github 설정 변경 [#165]

--- module-api/src/main/java/com/programmers/dev/configuration/SecurityConfiguration.java
+++ module-api/src/main/java/com/programmers/dev/configuration/SecurityConfiguration.java
@@ -57,6 +57,7 @@ public class SecurityConfiguration {
                                 .requestMatchers(AntPathRequestMatcher.antMatcher("/user/me")).hasRole("USER")
                                 .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")).permitAll()
                                 .requestMatchers(AntPathRequestMatcher.antMatcher("/web/myPage")).permitAll()
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/docs/index.html")).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtConfigure), AnonymousAuthenticationFilter.class)
